03a72143b15205b88cc1ac37ed7a492cab3edda4
陈贵 <chengui@reapal.com>
2017-02-08 14:49:16 +0800

    Revert: bug

--- user-provider/src/test/java/UserServiceTest.java
+++ user-provider/src/test/java/UserServiceTest.java
@@ -1,5 +1,6 @@
 
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.plugins.Page;
 import com.baomidou.mybatisplus.toolkit.Sequence;
 import com.reapal.model.User;
 import com.reapal.service.UserService;
@@ -26,18 +27,74 @@ public class UserServiceTest {
     @Test
     public void testInsert() {
         Sequence sequence = new Sequence();
-        User user = new User(sequence.nextId(),"jack-cooper",new Random().nextInt(80));
-        boolean insert = userService.insert(user);
-        System.out.println("insert = " + insert);
+        for(int i =1000 ;i < 1005; i++) {
+            User user = new User(new Integer(i).longValue(), "jack-cooper", new Random().nextInt(80));
+            boolean insert = userService.insert(user);
+        }
     }
 
     @Test
-    public void test() {
+    public void testSelect() {
         User u = new User();
-        u.setName("jack-cooper");
+        u.setAge(33);
         List<User> users = userService.selectList(new EntityWrapper<User>(u));
         System.out.println("=========================== " + userService.selectList(new EntityWrapper<User>(u)));
         System.out.println("******************************= " + userService.selectCount(new EntityWrapper<User>(null)));
+    }
+
+    @Test
+    public void testUpdate(){
+        User u = new User();
+        u.setId(0l);
+        u.setName("bbb");
+        u.setAge(99);
+        userService.updateById(u);
+    }
+
+    @Test
+    public void testSlectByPage(){
+        User u = new User();
+        u.setId(0l);
+        u.setName("bbb");
+        u.setAge(99);
+        Page<User> page = new Page<User>();
+        Page<User> userPage = userService.selectPage(page);
+        System.out.println("userPage = " + userPage);
+        System.out.println("userPage = " + userPage.getRecords());
+    }
+
+
+    @Test
+    public void testSlectByLike(){
+        List<User> users = userService.selectList(
+                new EntityWrapper<User>().like("name", "oo")
+//                        .and("age={0}", "10")
+                        .orderBy("id")
+        );
+        System.out.println("users = " + users);
+        System.out.println("size = " + users.size());
+    }
+
+    @Test
+    public void testSlectBetween(){
+        List<User> users = userService.selectList(
+                new EntityWrapper<User>().like("name", "oo")
+                        .between("age", "10","20")
+                        .orderBy("id")
+        );
+        System.out.println("users = " + users);
+        System.out.println("size = " + users.size());
+    }
+
+    @Test
+    public void testSlectIn(){
+        List<User> users = userService.selectList(
+                new EntityWrapper<User>().like("name", "oo")
+                        .in("age", new String[]{"10","11"})
+                        .orderBy("id")
+        );
+        System.out.println("users = " + users);
+        System.out.println("size = " + users.size());
     }
 
 }
